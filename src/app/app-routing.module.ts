ecc40ec1875eddffc87ae85f6a93ddad488a4d30
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-11-27 20:09:08 +0400

    adding urlhash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports:[RouterModule]
 })
 export class AppRoutingModule { }
